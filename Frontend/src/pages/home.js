0d8a11070860e2cc64308e1b148c065dce8af918
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-02 16:35:01 +0530

    Refactor API call and improve job mapping

--- Frontend/src/pages/home.js
+++ Frontend/src/pages/home.js
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import API from "../api";
 
 function Home() {
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    axios.get("http://YOUR_EC2_IP:5000/api/jobs")
-      .then(res => setJobs(res.data));
+    API.get("/jobs").then(res => setJobs(res.data));
   }, []);
 
   return (
     <div className="container">
       <h1>🚀 Job Portal</h1>
 
-      {jobs.map((job, i) => (
-        <div className="card" key={i}>
+      {jobs.map(job => (
+        <div className="card" key={job._id}>
           <h3>{job.title}</h3>
           <p>{job.location}</p>
         </div>

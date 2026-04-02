cad9d5d4f7ff1d2dc94734c710fd7a4deb3cc1d4
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-02 14:59:30 +0530

    Refactor App component to use React Router

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -1,21 +1,19 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+import Certificate from "./pages/Certificate";
 
 function App() {
-  const [jobs, setJobs] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://YOUR_EC2_IP:5000/api/jobs")
-      .then(res => setJobs(res.data));
-  }, []);
-
   return (
-    <div>
-      <h1>Job Portal</h1>
-      {jobs.map((job, i) => (
-        <p key={i}>{job.title}</p>
-      ))}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/certificate" element={<Certificate />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

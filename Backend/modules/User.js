5ee12391b39c033566f06a554749cd697bd2d615
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-02 14:20:25 +0530

    Fix UserSchema definition and export statement

--- Backend/modules/User.js
+++ Backend/modules/User.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
-  password: String,
-  role: { type: String, enum: ["user", "employer"] }
+  password: String
 });
 
 module.exports = mongoose.model("User", UserSchema);

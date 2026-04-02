6b30c024c490a451678c40f17a2c2a03a0656819
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-02 15:06:00 +0530

    Implement login functionality in login.js

---
+++ Frontend/src/pages/login.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const res = await axios.post("http://YOUR_EC2_IP:5000/api/auth/login", {
+      email,
+      password
+    });
+
+    localStorage.setItem("token", res.data.token);
+    window.location = "/admin";
+  };
+
+  return (
+    <div className="container">
+      <h2>Login</h2>
+
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
+
+export default Login;

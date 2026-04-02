f9afe4f307d666f98016d8f68a21391f7abe597f
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-02 16:35:28 +0530

    Refactor job addition to use API module

--- Frontend/src/pages/admin.js
+++ Frontend/src/pages/admin.js
@@ -1,15 +1,17 @@
 import { useState } from "react";
-import axios from "axios";
+import API from "../api";
 
 function Admin() {
   const [title, setTitle] = useState("");
   const token = localStorage.getItem("token");
 
   const addJob = async () => {
-    await axios.post(
-      "http://YOUR_EC2_IP:5000/api/jobs",
-      { title },
-      { headers: { Authorization: token } }
+    await API.post(
+      "/jobs",
+      { title, location: "India" },
+      {
+        headers: { Authorization: token }
+      }
     );
 
     alert("Job Added");
@@ -19,7 +21,7 @@ function Admin() {
     <div className="container">
       <h2>Admin Dashboard</h2>
 
-      <input placeholder="Job Title" onChange={e => setTitle(e.target.value)} />
+      <input onChange={e => setTitle(e.target.value)} placeholder="Job Title" />
       <button onClick={addJob}>Add Job</button>
     </div>
   );

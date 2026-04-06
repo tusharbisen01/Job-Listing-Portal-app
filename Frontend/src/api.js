4be43a8231673d73bda19d0ec23d66373be7d58c
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-06 13:37:13 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://3.26.0.147:5000"
+  baseURL: "http://3.26.0.147:5000/api"
+});
+
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) req.headers.Authorization = token;
+  return req;
 });
 
 export default API;

e2b7d54f144a8e28da7fb87e0107186edde00159
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-06 15:37:20 +0530

    Update auth.js

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,13 +1,15 @@
 const jwt = require("jsonwebtoken");
 
-function auth(req, res, next) {
+module.exports = function (req, res, next) {
   const token = req.header("Authorization");
-  if (!token) return res.status(401).send("Access denied");
 
-  const verified = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = verified;
+  if (!token) return res.status(401).json({ msg: "No token" });
 
-  next();
-}
-
-module.exports = auth;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ msg: "Invalid token" });
+  }
+};

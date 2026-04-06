c5ae5fd619a455cb45d942e311aff85fdb88d325
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-06 14:39:44 +0530

    Update and rename login.jsx to Login.jsx

---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import API from "../api";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await API.post("/auth/login", { email, password });
+      localStorage.setItem("token", res.data.token);
+      alert("Login successful");
+    } catch (err) {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <div className="center">
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input type="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}

d6e5b9c5e97144cb65a1ad7b2bcca1e5de23dcef
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-06 15:44:24 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,22 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
-const authRoutes = require("./routes/authRoutes");
-const jobRoutes = require("./routes/jobRoutes");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/jobs", jobRoutes);
-
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on ${process.env.PORT}`)
-);
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/jobs", require("./routes/jobRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("Backend running 🚀");
+});
+
+app.listen(5000, "0.0.0.0", () => console.log("Server running"));

8f0980a5fd904211912cec652095f8cf03a3b520
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-06 15:38:42 +0530

    Update jobRoutes.js

--- Backend/routes/jobRoutes.js
+++ Backend/routes/jobRoutes.js
@@ -3,17 +3,24 @@ const router = express.Router();
 const Job = require("../models/Job");
 const auth = require("../middleware/auth");
 
-// Get jobs (protected)
+// Public jobs
 router.get("/", async (req, res) => {
   const jobs = await Job.find();
   res.json(jobs);
 });
 
-// Add job (optional)
-router.post("/", async (req, res) => {
-  const job = new Job(req.body);
-  await job.save();
-  res.send("Job added");
+// Apply job
+router.post("/apply", auth, async (req, res) => {
+  const Application = require("../models/Application");
+
+  const app = new Application({
+    userId: req.user.id,
+    jobId: req.body.jobId,
+    resume: req.body.resume
+  });
+
+  await app.save();
+  res.json({ msg: "Applied successfully" });
 });
 
 module.exports = router;

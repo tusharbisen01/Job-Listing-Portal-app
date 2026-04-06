346baa5014661172bf8a169b74af2e9a70283230
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-06 14:40:05 +0530

    Update Jobs.jsx

--- Frontend/src/pages/Jobs.jsx
+++ Frontend/src/pages/Jobs.jsx
@@ -1,17 +1,22 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import API from "../api";
 
 export default function Jobs() {
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    API.get("/jobs").then(res => setJobs(res.data));
+    API.get("/jobs")
+      .then(res => setJobs(res.data))
+      .catch(err => console.log(err));
   }, []);
 
   return (
     <div>
+      <h2>Available Jobs</h2>
       {jobs.map(job => (
-        <div key={job._id}>{job.title}</div>
+        <div key={job._id} className="card">
+          {job.title}
+        </div>
       ))}
     </div>
   );

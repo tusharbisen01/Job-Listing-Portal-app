0f6a869eaecc93a3ca644eb51ee0136b34e522bb
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-06 13:32:00 +0530

    Update and rename jobs.js to jobRoutes.js

---
+++ Backend/routes/jobRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const Job = require("../models/Job");
+const auth = require("../middleware/auth");
+
+// Get jobs (protected)
+router.get("/", auth, async (req, res) => {
+  const jobs = await Job.find();
+  res.json(jobs);
+});
+
+// Add job (optional)
+router.post("/", async (req, res) => {
+  const job = new Job(req.body);
+  await job.save();
+  res.send("Job added");
+});
+
+module.exports = router;

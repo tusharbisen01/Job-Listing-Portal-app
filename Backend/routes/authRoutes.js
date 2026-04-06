167fa7c5701f844233209d531d6fef55d3eb1238
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-06 13:31:15 +0530

    Update and rename auth.js to authRoutes.js

---
+++ Backend/routes/authRoutes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// Register
+router.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = new User({ email, password: hashed });
+  await user.save();
+
+  res.send("User registered");
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).send("User not found");
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).send("Invalid credentials");
+
+  const token = jwt.sign(
+    { id: user._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+
+  res.json({ token });
+});
+
+module.exports = router;

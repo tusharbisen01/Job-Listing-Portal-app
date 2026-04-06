15e48dadc79acd9d51bbda9c07137198431cb24b
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-06 13:38:11 +0530

    Update Jobs.jsx

--- Frontend/src/pages/Jobs.jsx
+++ Frontend/src/pages/Jobs.jsx
@@ -1,1 +1,18 @@
+import { useEffect, useState } from "react";
+import API from "../api";
 
+export default function Jobs() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    API.get("/jobs").then(res => setJobs(res.data));
+  }, []);
+
+  return (
+    <div>
+      {jobs.map(job => (
+        <div key={job._id}>{job.title}</div>
+      ))}
+    </div>
+  );
+}

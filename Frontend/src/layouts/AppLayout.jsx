cb0f2cf832adb82c962b1148982ee2263935afad
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-07 15:20:05 +0530

    Update AppLayout.jsx

--- Frontend/src/layouts/AppLayout.jsx
+++ Frontend/src/layouts/AppLayout.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import { Outlet } from "react-router-dom";
 
+export default function AppLayout() {
+  return (
+    <div className="layout">
+      <Sidebar />
+      <main className="content">
+        <Outlet />
+      </main>
+    </div>
+  );
+}

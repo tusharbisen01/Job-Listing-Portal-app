eae986a8a3b4b62617289923e42bf00e658854d3
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-07 15:40:58 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ThemeToggle from "./ThemeToggle";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
+    <div className="navbar glass">
       <h2>JobPortal</h2>
+
       <div>
         <Link to="/">Home</Link>
         <Link to="/jobs">Jobs</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/login" className="btn">Login</Link>
+        <Link to="/login">Login</Link>
+        <ThemeToggle />
       </div>
-    </nav>
+    </div>
   );
 }

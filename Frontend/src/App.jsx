125ac536fc92792e473ae3838a91f036fd30b25f
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-07 15:47:23 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,31 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import PublicLayout from "./layouts/PublicLayout";
-import AppLayout from "./layouts/AppLayout";
-
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 import Jobs from "./pages/Jobs";
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* 🌐 PUBLIC ROUTES */}
-        <Route element={<PublicLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Route>
-
-        {/* 🔐 APP ROUTES */}
-        <Route element={<AppLayout />}>
-          <Route path="/jobs" element={<Jobs />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );

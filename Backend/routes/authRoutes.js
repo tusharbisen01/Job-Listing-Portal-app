0c30e4257d532112006778c363bf5dd1ecd9ba0a
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-07 23:50:06 +0530

    Update authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../modules/User");
 
 /* ================= REGISTER ================= */
 router.post("/register", async (req, res) => {

d72d3d700f8680611bb57d5d9fe2a1e7d141cb93
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-08 17:12:17 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://3.107.165.203:5000/api"
+  baseURL: "http://16.176.32.70:5000/api"
 });
 
 API.interceptors.request.use((req) => {

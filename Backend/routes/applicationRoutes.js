632131e8f9acb5d00794483124ab567c02afab0e
Tushar Bisen <tusharbisen917215@gmail.com>
2026-04-08 13:17:27 +0530

    Update applicationRoutes.js

--- Backend/routes/applicationRoutes.js
+++ Backend/routes/applicationRoutes.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const Application = require("../modules/Application");
+const auth = require("../middleware/auth");
 
+/* APPLY */
+router.post("/apply/:jobId", auth, async (req, res) => {
+  const app = new Application({
+    userId: req.user.id,
+    jobId: req.params.jobId,
+  });
+
+  await app.save();
+  res.send("Applied");
+});
+
+/* MY APPLICATIONS */
+router.get("/my-applications", auth, async (req, res) => {
+  const apps = await Application.find({ userId: req.user.id });
+  res.json(apps);
+});
+
+module.exports = router;
